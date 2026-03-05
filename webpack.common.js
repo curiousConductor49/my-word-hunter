786b5eb1ceb3ab816afed6f1e22914812ac1249f
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-03-05 12:35:48 -0500

    chore: install loaders for css and image display

--- webpack.common.js
+++ webpack.common.js
@@ -14,6 +14,22 @@ export default {
       title: 'Production',
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      }
+    ],
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(import.meta.dirname, 'dist'),
