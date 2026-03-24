e9197e6ce3d36ef5ea6779e594ec021ee46c6423
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-03-24 15:34:06 -0400

    feat: import Game class and remove old commented-out code

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
 import './styles.css'
 import magnify from './assets/light-theme-magnify.svg'
 import gameData from './gameData.json'
+import Game from './gameClass.js'
 // import theme from './assets/theme-light-dark.svg'
 
 // main index.js content
@@ -27,47 +28,9 @@ const targetSentence = document.querySelector("#target-sentence");
 const dummyWordsGroup = document.querySelector("#dummy-words-group");
 const playGameBtn =  document.querySelector("#play-game-btn");
 
-// function definitions
-function populateDivsWithData(wordGroupDiv, targetSentenceDiv, arrOfRounds, currentRound) {
-    // populate empty dom container for target sentence
-    const completeSentenceStr = arrOfRounds[currentRound]["missing words"].join("|");
-    const missingWordsRegex = new RegExp(completeSentenceStr, "gi");
-    targetSentenceDiv.innerHTML = arrOfRounds[currentRound]["complete sentence"].replaceAll(missingWordsRegex, "_");
-    
-    // populate empty dom container for word group
-    const wordGroupContent = arrOfRounds[currentRound]["dummy words"]
-    .map((word) => `<span tabindex="0" class="dummy-word">${word}</span>`);
-    const finalContent = arrOfRounds[currentRound]["missing words"].reduce((accumulator, currentValue) => {
-        const minIndex = 1;
-        const maxIndex = wordGroupContent.length - 1;
-        const randomIndex = Math.floor(Math.random() * (maxIndex - minIndex + 1) + minIndex);
-        accumulator.splice(randomIndex, 0, `<span tabindex="0" class="target-word">${currentValue}</span>`);
-        return accumulator;
-    }, wordGroupContent);
-    wordGroupDiv.innerHTML = finalContent.join(" ");
-}
-
-function findMissingWord(event, targetSentenceDiv, arrOfRounds, currentRound) {
-    if (arrOfRounds[currentRound]["missing words"].includes(event.target.innerText)) {
-
-        // would be good to implement a check to see if the clicked word has already been "found", so the replacement doesn't run unnecessarily
-        const wordIndex = arrOfRounds[currentRound]["complete sentence"]
-        .split(" ")
-        .findIndex((word) => word === event.target.innerText);
-        
-        const currentSentenceContent = targetSentenceDiv.textContent;
-        targetSentenceDiv.textContent = currentSentenceContent
-        .split(" ")
-        .toSpliced(wordIndex, 1, event.target.textContent)
-        .join(" ");
-
-        console.log(targetSentenceDiv.textContent.split(" ").toSpliced(wordIndex, 1, event.target.textContent).join(" "));
-    }
-}
-
-// test function calls
-// populateDivsWithData(dummyWordsGroup, targetSentence, gameData, 1);
-
-// dummyWordsGroup.addEventListener("click", (event) => {
-//   findMissingWord(event, targetSentence, gameData, 1);
-// })
+// gameplay
+playGameBtn.addEventListener("click", (event) => {
+    const instance = new Game();
+    instance.populateTargetSentenceContainer(gameData, 1, targetSentence);
+    instance.populateDummyWordsContainer(gameData, 1, dummyWordsGroup);
+})
