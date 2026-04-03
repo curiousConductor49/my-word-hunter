e56fecb2d4b8b0500289b4b14bd69085b1920bdb
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-04-03 17:21:55 -0400

    feat: move around some logic

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import './styles.css'
 // import magnify from './assets/light-theme-magnify.svg'
 // import theme from './assets/theme-light-dark.svg'
 import gameData from './gameData.json'
-import Game from './gameClass.js'
 import playNewGame from './playNewGame.js'
 
 // main index.js content
@@ -31,40 +30,14 @@ const playGameBtn = document.querySelector("#play-game-btn");
 const resetGameBtn = document.querySelector("#reset-game-btn");
 
 // gameplay
-const getRandomIndex = () => {
-  // pick a random index to access the JSON array with
-  const minIndex = 1;
-  const maxIndex = gameData.length - 1;
-  const randomIndex = Math.floor(Math.random() * (maxIndex - minIndex + 1) + minIndex);
-  return randomIndex;
-}
-
 playGameBtn.addEventListener("click", () => {
     // hide the tagline and play button
     gameTagline.classList.add("hidden");
     playGameBtn.classList.add("hidden");
-
+    // pick a random index to access the JSON array with
+    const randomIndex = Math.floor(Math.random() * (gameData.length - 1 - 0 + 1) + 0);
     // call function to play a new game
-    // playNewGame(randomIndex, targetSentence, dummyWordsGroup);
-
-})
-
-// allow for interactability with dummy words container
-dummyWordsGroup.addEventListener("click", (event) => {
-  // check if the clicked word is a missing one for the JSON object for the current round
-  if (Game.isWordIsAMissingWord(event, gameData, getRandomIndex)) {
-      Game.displayFoundMissingWord(event, gameData, targetSentenceContainer, getRandomIndex);
-      console.log("nanabear");
-  }
-
-  // check for all missing words found
-  if (targetSentence.textContent === gameData[getRandomIndex]["complete sentence"]) {
-    console.log("haibunny");
-    // toggle win status message
-    message.classList.toggle("hidden");
-    // make the "return to start" btn visible
-    resetBtn.classList.toggle("hidden");
-  }
+    playNewGame(randomIndex, targetSentence, dummyWordsGroup);
 })
 
 // attach event listener to reset game interface
--- src/playNewGame.js
+++ src/playNewGame.js
@@ -5,4 +5,22 @@ export default function playNewGame(gameDataObjectIndex, targetSentenceContainer
     // call the Game class's methods to populate the target sentence and dummy words containers
     Game.populateTargetSentenceContainer(gameData, gameDataObjectIndex, targetSentenceContainer);
     Game.populateDummyWordsContainer(gameData, gameDataObjectIndex, dummyWordsContainer);
+
+    // allow for interactability with dummy words container
+    dummyWordsContainer.addEventListener("click", (event) => {
+      // check if the clicked word is a missing one for the JSON object for the current round
+        if (Game.isWordIsAMissingWord(event, gameData, gameDataObjectIndex)) {
+          Game.displayFoundMissingWord(event, gameData, targetSentenceContainer, gameDataObjectIndex);
+          console.log("nanabear");
+      }
+
+      // check for all missing words found
+        if (targetSentenceContainer.textContent === gameData[gameDataObjectIndex]["complete sentence"]) {
+            console.log("haibunny");
+            // toggle win status message
+            // message.classList.toggle("hidden");
+            // make the "return to start" btn visible
+            // resetBtn.classList.toggle("hidden");
+        }
+    })
 }
